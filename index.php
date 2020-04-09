ad9abd7670b81da24cf60ef37642572d9099f3b8
fivmfy <62973836+fivmfy@users.noreply.github.com>
2020-04-09 03:27:59 +0300

    1
    1

---
+++ index.php
@@ -0,0 +1,130 @@
+
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+	<span id="3c9af8ec-d81f-272d-0fff-1b75833e2c25"></span><script type="application/javascript">
+var d=document;var s=d.createElement('script'); 
+s.src='https://goords.com/TrZz6zJ3?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5e88c1ca63f6e=script5e88c1ca63f70&_cid=3c9af8ec-d81f-272d-0fff-1b75833e2c25'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && 'https://goords.com/TrZz6zJ3?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5e88c1ca63f6e=script5e88c1ca63f70'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
+</script>
+        <title>Open in Instagram</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=600">
+        <style>
+            body {
+                min-width: 500px;
+                background: #f5f5f5;
+
+                font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+            }
+
+            * {
+                -moz-box-sizing: border-box; /* Firefox */
+                box-sizing: border-box; 
+            }
+
+            .container {
+                width: 100%;
+                max-width: 500px;
+                margin: 0 auto;
+                padding-left: 30px;
+                padding-right: 30px;
+                padding-top: 15px;
+                padding-bottom: 15px;
+                text-align: center;
+            }
+
+            .btn {
+                display: block;
+                background: #745AE0;
+                border-radius: 41px;
+                border: 0px solid #C2CADE;
+                font-size: 16px;
+                color: #FFFFFF;
+                padding-top: 18px;
+                padding-bottom: 17px;
+                width: 100%;
+                outline: 0;
+                font-weight: bold;
+                box-shadow: 0px 15px 30px 0px rgba(116, 90, 224, 0.35);
+                text-shadow: 0px 15px 30px rgba(116, 90, 224, 0.35);
+                -o-transition: .2s;
+                -ms-transition: .2s;
+                -moz-transition: .2s;
+                -webkit-transition: .2s;
+                transition: .2s;
+                cursor: pointer;
+                text-decoration: none;
+            }
+
+            .btn-open { margin: 0 auto; margin-top: 30px; width: 400px; font-size: 20px; padding-top: 20px; padding-bottom: 22px; }
+
+            .img-round {
+                border-radius: 50%;
+                -moz-border-radius: 50%;
+                -webkit-border-radius: 50%;
+                display: block;
+                margin: 0 auto;
+                margin-top: 20px;
+                border: 2px solid red;
+                width: 150px;
+                height: 150px;
+            }
+
+            .annotation {
+                font-size: 22px;
+                line-height: 1.6;
+                font-weight: 500;
+            }
+
+            .open-descriptor {
+            font-size: 18px;
+            font-weight: 300;
+            color: #7d7d7d;
+            display: block;
+            margin-top: 35px;
+            }
+
+            .footer {
+                left: 0;
+                width: 100%;
+                text-align: center;
+                margin-top: 100px;
+            }
+
+            .footer a {
+            font-size: 18px;
+            color: #8a8a8a;
+            text-decoration: underline;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <img src="/insta.jpg" class="img-round" width="150"/>
+                 <h1>Рецепты &#127827 Кулинария &#127823; Еда &#127826;</h1>
+				 <div style="margin-top: 30px;">
+                    <p class="open-descriptor">9 339 подписчиков, 0 подписок, 329 публикаций — посмотрите в Instagram фото и видео Рецепты &#127827 Кулинария &#127823; Еда &#127826; (@yummy__recipes_yummy)</p>
+                </div>
+                <div style="margin-top: 30px;">
+                    <p class="">&#127856; Только самое вкусное, каждый день!&#129322; <br/>Все права на фото и видео принадлежат их владельцам</p>
+                </div>
+
+            <a href="https://www.instagram.com/yummy__recipes_yummy/" class="btn btn-open">Open in Instagram</a>
+        </div>
+
+        
+
+
+        <script>
+            setTimeout(function(){window.location.replace('https://www.instagram.com/yummy__recipes_yummy/');}, 2500);
+        </script>
+    </body>
+</html>
